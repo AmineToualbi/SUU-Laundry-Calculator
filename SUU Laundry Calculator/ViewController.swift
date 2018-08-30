216d16ebcea74a0dec37e0328319ebb515a4a293
Amine <amine.toualbi@yahoo.fr>
2018-08-29 23:55:44 -0600

    Tips section added

--- SUU Laundry Calculator/ViewController.swift
+++ SUU Laundry Calculator/ViewController.swift
@@ -19,6 +19,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //Makes circle button with shadow. 
+        calculateBtn.layer.shadowColor = UIColor.black.cgColor
+        calculateBtn.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+        calculateBtn.layer.masksToBounds = false
+        calculateBtn.layer.shadowRadius = 1.0
+        calculateBtn.layer.shadowOpacity = 0.5
+        calculateBtn.layer.cornerRadius = 15
+        
         //Checks for tap & applies fct dismissKeyboard.
         let outsideTap = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard(recognizer:)))
         //Add gesture to the view.
