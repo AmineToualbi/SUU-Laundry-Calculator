80445198d93db5ea1b13af9248b03c33929926b0
Amine <amine.toualbi@yahoo.fr>
2018-11-02 00:47:02 -0600

    Added labels for better UX

--- SUU Laundry Calculator/Controller/SecondViewController.swift
+++ SUU Laundry Calculator/Controller/SecondViewController.swift
@@ -15,15 +15,20 @@ class SecondViewController: UIViewController {
     @IBOutlet weak var dryLabel: UILabel!
     @IBOutlet weak var washLabel: UILabel!
     
+    @IBOutlet weak var washPriceLabel: UILabel!
+    @IBOutlet weak var dryPriceLabel: UILabel!
+    @IBOutlet weak var changeLabel: UITextView!
+    
     var budget : Double = 0
     var budgetAmount : Int = 0
     var washPrice : Double = 0
     var dryPrice : Double = 0
     var dormChoice : String = ""
-    var washNbr : Int = 0
-    var dryNbr : Int = 0
+    var washNbr : Double = 0
+    var dryNbr : Double = 0
     var washBudget : Double = 0
     var dryBudget : Double = 0
+    var change : Double = 0
     
     
     override func viewDidLoad() {
@@ -50,18 +55,37 @@ class SecondViewController: UIViewController {
             dryPrice = 1.00
         }
         
-        budget = floor(budget)
-        print(budget)
+        let roundedBudget : Double = floor(budget)
+//        print(budget)
+//        print(roundedBudget)
         
-        washBudget = budget / 2
-        dryBudget = budget / 2
         
-        washNbr =  Int(floor(washBudget / washPrice))
-        dryNbr = Int(floor(dryBudget / dryPrice))
+        washBudget = roundedBudget / 2
+        dryBudget = roundedBudget / 2
         
-        washLabel.text = String(washNbr)
-        dryLabel.text = String(dryNbr)
+//        print("Wash budget = " + String(washBudget))
+//        print("Dry budget = " + String(dryBudget))
         
+        washNbr =  floor(washBudget / washPrice)
+        dryNbr = floor(dryBudget / dryPrice)
+        
+//        print("Wash nbr = \(washNbr)")
+//        print("Dry nbr = \(dryNbr)")
+//        print("dryNbr * dryPrice = \(dryNbr*dryPrice)")
+//      //  print(Double(washNbr*washPrice))
+//        //print(dryNbr*dryPrice)
+    
+        change = budget - (washNbr * washPrice + dryNbr * dryPrice)
+        print("BUDGET IS " + String(budget))
+        print ("CHANGE IS " + String(change))
+        
+        changeLabel.text = "   Change: $" + (String(format: "%.2f", change))
+        
+        washLabel.text = String(Int(washNbr))
+        dryLabel.text = String(Int(dryNbr))
+        
+        washPriceLabel.text = "Wash = $" + (String(format: "%.2f", washPrice))
+        dryPriceLabel.text = "Dry = $" + (String(format: "%.2f", dryPrice))
 
     }
     
@@ -70,12 +94,5 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-   
- 
- 
-    
-    
-    
-    
-    
+
 }
